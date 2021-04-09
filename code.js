3b9a86d1adbb159df2773ecc29d2ebb524cc13fe
MichalKolban <35864270+MichalKolban@users.noreply.github.com>
2021-04-09 16:44:35 +0200

    ver 1.1

--- code.js
+++ code.js
@@ -1,20 +1,22 @@
 const btn = document.querySelector('button');
 
-const calculateBill = () =>{
-    event.preventDefault();
+const calculateBill = (e) =>{
+    e.preventDefault();
     let bill = document.querySelector('.bill').value;
     let people = document.querySelector('.peopleCount').value;
     let tip = document.querySelector('.percentage').value;
     const sum = document.querySelector('h2 span');
     bill = Number(bill);
     people = Number(people);
-    tip = Number(tip.substring(0, tip.length - 1));
+    tip = Number(tip);
     const result = calculate(bill,people,tip);
     sum.textContent = result;
-    console.log("result z pierwszego " + result);
 }
 
 function calculate(bill,people,tip) {
+    if((bill * 100/people) < 1){
+        return alert(`it's very strange to split ${bill} for ${people} peoples , but it less the 0.01 🤔`)
+    }
     if(people !== 0){
         let result = ((bill + ((bill * tip)/100)) / people).toFixed(2);
     return result;
